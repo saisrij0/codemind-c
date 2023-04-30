7873ae98de1bac0e6924ec2d583538fa58a9d541
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-04-30 12:11:03 +0530

    #include<stdio.h> int main() {     int n,r,e=0,o=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         if(r%2==0)         {             e=e+1;         }         else{             o=o+1;         }         n=n/10;     }     if(e>0 && o==0)     {         printf("Even");     }     else if(e==0 && o>0)     {         printf("Odd");     }     else{         printf("Mixed");     } }

---
+++ Even_Odd_Mixed.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,e=0,o=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        if(r%2==0)
+        {
+            e=e+1;
+        }
+        else{
+            o=o+1;
+        }
+        n=n/10;
+    }
+    if(e>0 && o==0)
+    {
+        printf("Even");
+    }
+    else if(e==0 && o>0)
+    {
+        printf("Odd");
+    }
+    else{
+        printf("Mixed");
+    }
+}
