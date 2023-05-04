661fd046ce6518414e7c6e82b5f27c324811b6ba
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-05-04 20:54:54 +0530

    #include<stdio.h> int main() {     while(1)     {         int n;         scanf("%d",&n);         int sq;         sq=n*n;         if(n!=-1)         {             printf("%d ",sq);         }         else{             break;         }     } }

---
+++ Infinite.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        int n;
+        scanf("%d",&n);
+        int sq;
+        sq=n*n;
+        if(n!=-1)
+        {
+            printf("%d
+",sq);
+        }
+        else{
+            break;
+        }
+    }
+}
