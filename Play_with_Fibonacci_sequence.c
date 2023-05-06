44e30612b13c3d6bd7f0f13d02504c84b9f91f45
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-05-06 15:24:09 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c;     scanf("%d",&n);     printf("%d %d ",a,b);     while(n-2>0)     {         c=a+b;         a=b;         b=c;         printf("%d ",c);         n--;     } }

---
+++ Play_with_Fibonacci_sequence.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,c;
+    scanf("%d",&n);
+    printf("%d %d ",a,b);
+    while(n-2>0)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+        printf("%d ",c);
+        n--;
+    }
+}
