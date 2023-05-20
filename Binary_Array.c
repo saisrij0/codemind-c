e1c4b648184b294496c99742b14013c189799701
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-05-20 11:53:31 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]==1 || a[i]==0)         {             c=c+1;         }     }     if(c==n)     {         printf("True");     }     else{         printf("False");     } }

---
+++ Binary_Array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==1 || a[i]==0)
+        {
+            c=c+1;
+        }
+    }
+    if(c==n)
+    {
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+}
