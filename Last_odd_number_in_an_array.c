b73afba4802e96be09bad637ddec09609d8e1955
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-05-22 20:49:37 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=n-1;i>=0;i--)     {         if(a[i]%2!=0)         {             break;         }     }     printf("%d",a[i]); }

---
+++ Last_odd_number_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]%2!=0)
+        {
+            break;
+        }
+    }
+    printf("%d",a[i]);
+}
