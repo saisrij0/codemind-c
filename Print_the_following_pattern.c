696cffa797cd2e6bede11f801974d33ed4e4b3ae
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-05-24 18:01:14 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,j;     for(i=n;i>0;i--)     {         for(j=1;j<=n;j++)         {             if(j==i|| j==n+1-i)             {                 printf("%d ",i);             }             else{                 printf(" ");             }         }         printf(" ");     } }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int a[n],i,j;
+    for(i=n;i>0;i--)
     {
-        for(j=i;j<=n;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d ",j);
+            if(j==i|| j==n+1-i)
+            {
+                printf("%d ",i);
+            }
+            else{
+                printf(" ");
+            }
         }
         printf("
 ");
