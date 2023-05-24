cc0de042cdf2f69134d2987a1f54c657183c5642
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-05-24 16:44:24 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,s=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             break;         }         s=s+a[i];     }     printf("%d",s); }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,s=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            break;
+        }
+        s=s+a[i];
+    }
+    printf("%d",s);
+}
