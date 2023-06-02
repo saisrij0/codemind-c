0e91f5d014ee5fb3fe6c5396fe093d420a7ef1ef
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-06-02 16:07:45 +0530

    #include<stdio.h> int prime(int n) {     int i,c=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else{         return 0;     } } int main() {     int n;     scanf("%d",&n);     int a[n],i,c=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int m;     scanf("%d",&m);     for(i=0;i<n;i++)     {         if(prime(a[i]))         {             if(a[i]<=m)             {                 c++;             }         }     }     printf("%d",c); }

---
+++ primes_less_than_k.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int i,c=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int m;
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        if(prime(a[i]))
+        {
+            if(a[i]<=m)
+            {
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
