816d770267f767276d5a658c1b13aaa215c0741f
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-06-02 16:26:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,f=0,l=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i<n/2)         {             f=f+a[i];         }         else{             l=l+a[i];         }     }     if(l>=f)     {         printf("%d",l-f);     }     else{         printf("%d",f-l);     } }

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,f=0,l=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i<n/2)
+        {
+            f=f+a[i];
+        }
+        else{
+            l=l+a[i];
+        }
+    }
+    if(l>=f)
+    {
+        printf("%d",l-f);
+    }
+    else{
+        printf("%d",f-l);
+    }
+}
